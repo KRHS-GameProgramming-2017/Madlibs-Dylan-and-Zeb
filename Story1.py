1c72a74fe4f96c14cd0f4ab1c855901d2a76da55
Dylan Monette <dylan.monette@kearsarge.org>
2017-10-12 12:59:29 -0400

    start

---
+++ Story1.py
@@ -0,0 +1,18 @@
+from Getter import *
+def story1(debug= False):
+	if debug: print "--In story1 function--"
+	
+	friend1 = getWord("A Name: ", debug)
+	distance1 = getNumber("A Number: ", debug)
+	store1 = getWord("A Store: ", debug)
+	
+	out = ""
+	out += "When the  " + Device1
+	out += ", shut down, the" + Mythical_Beast 1
+	out += " got upset and " + Swear
+	out += "ran out. "
+	
+	
+	return out
+    
+    
