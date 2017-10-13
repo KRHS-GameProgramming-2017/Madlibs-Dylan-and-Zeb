43b88e06f38825118c5e911d782cd0c979d7d337
Matternix <Zeb.santos@kearsarge.org>
2017-10-13 13:29:23 -0400

    Edited

--- Screens.py
+++ Screens.py
@@ -5,7 +5,7 @@ def splash(debug = False):
 	output += "*                                     *\n"
 	output += "*        Welcome to Madlibs           *\n"
 	output += "*           Written by                *\n"
-	output += "*         Chris Spooner               *\n"
+	output += "*        Zebulon + Dylan              *\n"
 	output += "*                                     *\n"
 	output += "***************************************\n"
 	return output
