611a412d3f086bf184f902c99f9317a19b20881e
Matternix <Zeb.santos@kearsarge.org>
2017-10-13 13:52:58 -0400

    sd dfas

--- Story1.py
+++ Story1.py
@@ -3,20 +3,31 @@ from Getter import *
 # zeb waz heeer
 
 def story1(debug= False):
-	if debug: print "--In story1 function--"
-	
-	friend1 = getWord("A Name: ", debug)
-	distance1 = getNumber("A Number: ", debug)
-	store1 = getWord("A Store: ", debug)
-	
-	out = ""
-	out += "When the  " + Device1
-	out += ", shut down, the" + Mythical_Beast1
-	out += " got upset and " + Swear1 
-	out += "ran out." + place1
-    out += " 
-	
-	
-	return out
+    if debug: print "--In story1 function--"
+    
+    friend1 = getWord("A Name: ", debug)
+    distance1 = getNumber("A Number: ", debug)
+    store1 = getWord("A Store: ", debug)
+    
+    out = ""
+    out += "When the" + Device1
+    out += "shut down,the" + Mythical_Beast1
+    out += "got upset and" + Swear1 
+    out += "ran out." + place1
+    out = ""
+    out += "  " + 
+    out += "  " + 
+    out += "  " +  
+    out += "  " + 
+    
+    out = ""
+    out += "  " + 
+    out += "  " + 
+    out += "  " +  
+    out += "  " + 
+    
+    
+    
+    return out
     
     
