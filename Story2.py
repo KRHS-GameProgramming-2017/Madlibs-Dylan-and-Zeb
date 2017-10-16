b4dc65394bbbcff5bcbd387d220b3d92adad2d9d
Dylan Monette <dylan.monette@kearsarge.org>
2017-10-16 13:43:39 -0400

    started story2

---
+++ Story2.py
@@ -0,0 +1,30 @@
+from Getter import *
+
+# zeb waz heeer
+
+def story2(debug= False):
+    if debug: print "--In story1 function--"
+    
+    worker1= getWord("worker1: ", debug)
+    object1 = getWord("object1: ", debug)
+    body_part1 = getWord("body_part1: ", debug)
+    place1 = getWord("place1: ", debug)
+    emotion = getWord("emotion1: ", debug)
+    animal1 = getWord("
+    
+    
+    
+    
+    out = ""
+    out += "After i left the" + worker1
+    out += "Threw a" + Object1
+    out += "Which made a gash in my" + Body_part1
+    out += "worker1 took me to." + place1
+    out += "once i woke up i was" +emotion1
+    out += "while i was bleeding i noticed there was a" +animal1
+    out += "the animal1" +verb1
+    out +=
+    
+    return out
+    
+    
