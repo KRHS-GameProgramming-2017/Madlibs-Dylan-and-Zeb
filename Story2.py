8aff3852dd46727ed285e8f3133cdab9d7d7f44a
Dylan Monette <dylan.monette@kearsarge.org>
2017-10-19 13:37:29 -0400

    finished story 2 and edit

--- Story2.py
+++ Story2.py
@@ -9,23 +9,26 @@ def story2(debug= False):
     object1 = getWord("object1: ", debug)
     body_part1 = getWord("body_part1: ", debug)
     place1 = getWord("place1: ", debug)
-    emotion = getWord("emotion1: ", debug)
+    emotion1 = getWord("emotion1: ", debug)
     animal1 = getWord("animal1: ", debug)
     verb1 = getIngWord("verb that ends in 'ing': ", debug)
-    
+    action1 = getWord("action1: ", debug)
+    body_part1 = getWord("body_part1: ", debug)
+    weapon1 = getWord("weapon1: ", debug)
     
     
     
     out = ""
-    out += "After I left the" + worker1
-    out += "threw a" + object1
-    out += "which made a gash in my" + body_part1
-    out += worker1 + " took me to." + place1
-    out += "once I woke up I was" +emotion1
-    out += "while I was bleeding I noticed there was a" +animal
-    out += "the anima1" +verb1
-    out += ""
-    
+    out += " After I left the " + worker1
+    out += " threw a " + object1
+    out += " which made a gash in my " + body_part1
+    out += worker1 + " took me to. " + place1
+    out += " once I woke up I was " +emotion1
+    out += " while I was bleeding I noticed there was a " +animal1
+    out += " the anima1 started " +verb1
+    out += " me while I was trying to " +action1
+    out += " I had a huge gash and was bleeding from my " +body_part1
+    out += " I was thinking of escaping but he/she had a " +weapon1
     return out
     
     
