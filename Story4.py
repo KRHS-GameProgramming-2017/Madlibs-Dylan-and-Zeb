16c42f1feb41d9eac792369a86c9a2d5c08b041f
Matternix <Zeb.santos@kearsarge.org>
2017-10-20 13:46:11 -0400

    edited

--- Story4.py
+++ Story4.py
@@ -2,29 +2,60 @@ from Getter import *
 
 # zeb waz heeer
 
-def story3(debug= False):
+def story4(debug= False):
     if debug: print "--In story1 function--"
     
-    worker1= getWord("worker1: ", debug)
-    object1 = getWord("object1: ", debug)
-    body_part1 = getWord("body_part1: ", debug)
-    place1 = getWord("place1: ", debug)
-    emotion = getWord("emotion1: ", debug)
-    animal1 = getWord("animal1: ", debug)
-    verb1 = getIngWord("verb that ends in 'ing': ", debug)
-    
-    
-    
-    
-    out = ""
-    out += " After I left the " + worker1
-    out += " threw a " + object1
-    out += " which made a gash in my " + body_part1
-    out += worker1 + " took me to." + place1
-    out += " once I woke up I was " +emotion1
-    out += " while I was bleeding I noticed there was a " +animal
-    out += " the anima1" +verb1
-    out += ""
+    out = """
+          
+          
+          _ _,---._ 
+       ,-','       `-.___ 
+      /-;'               `._ 
+     /\/          ._   _,'o \ 
+    ( /\       _,--'\,','"`. ) 
+     |\      ,'o     \'    //\ 
+     |      \        /   ,--'""`-. 
+     :       \_    _/ ,-'         `-._ 
+      \        `--'  /                ) 
+       `.  \`._    ,'     ________,',' 
+         .--`     ,'  ,--` __\___,;' 
+          \`.,-- ,' ,`_)--'  /`.,' 
+           \( ;  | | )      (`-/ 
+             `--'| |)       |-/ 
+               | | |        | | 
+               | | |,.,-.   | |_ 
+               | `./ /   )---`  ) 
+              _|  /    ,',   ,-' 
+    Donuts   ,'|_(    /-<._,' |--, 
+             |    `--'---.     \/ \ 
+             |          / \    /\  \ 
+           ,-^---._     |  \  /  \  \ 
+        ,-'        \----'   \/    \--`. 
+       /            \              \   \ 
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+    """
     
     return out
     
