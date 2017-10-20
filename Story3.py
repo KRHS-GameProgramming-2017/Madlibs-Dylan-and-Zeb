986801dffac71783f4affa881364237749402d05
Dylan Monette <dylan.monette@kearsarge.org>
2017-10-20 13:39:58 -0400

    story 3

--- Story3.py
+++ Story3.py
@@ -5,23 +5,24 @@ from Getter import *
 def story3(debug= False):
     if debug: print "--In story1 function--"
     
-    worker1= getWord("worker1: ", debug)
+    = getWord(": ", debug)
     object1 = getWord("object1: ", debug)
-    body_part1 = getWord("body_part1: ", debug)
+    weapon1 = getWord("weapon1: ", debug)
     place1 = getWord("place1: ", debug)
     emotion = getWord("emotion1: ", debug)
     animal1 = getWord("animal1: ", debug)
     verb1 = getIngWord("verb that ends in 'ing': ", debug)
+    body_part1 = getWord("body_part1: ",debug)
     
     
     
     
-    out = ""
-    out += " After I left the " + worker1
-    out += " threw a " + object1
-    out += " which made a gash in my " + body_part1
-    out += worker1 + " took me to." + place1
-    out += " once I woke up I was " +emotion1
+    out = "I was walking down the" + place1
+    out += " ,until a " + animal1
+    out += " jumped on me and dragged me to " + place2
+    out += " once we stopped, there was a man that threw a " + weapon1
+    out += " at me.When I tried to run but a " + object1
+    out += " fell out of the sky and impaled in my  " +body_part1
     out += " while I was bleeding I noticed there was a " +animal
     out += " the anima1" +verb1
     out += ""
